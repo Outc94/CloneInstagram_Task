6cb6dfb954636f352577ea84fc46470fbcf753da
Mouhamed Diop <outc94@hotmail.fr>
2020-04-14 20:54:45 +0900

    Added Associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
+  mount_uploader :image, ImageUploader
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
+
+  validate :any_present?
+
+  def any_present?
+    if %w(texts image).all?{|attr| self[attr].blank?}
+      errors.add :base, "Both Fields Cannot be Blank. Fill in one Please"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  validates :full_name, presence: true, length: { maximum: 100 }
+  validates :username, presence:true, length: { maximum: 100 }
+  validates :email, presence:true, length: { maximum: 100 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  before_validation { email.downcase! }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 5 }
+  has_many :posts
+  has_many :profiles
+  has_many :favorites, dependent: :destroy
 end
