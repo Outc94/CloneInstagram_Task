32ceb4a21c68d4fa5e8be525207146258b24b0bc
Mouhamed Diop <outc94@hotmail.fr>
2020-04-15 21:52:04 +0900

    Instagram Clone with the views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+    resources :contacts
+    resources :profiles
+    resources :posts do
+      collection do
+        post :confirm
+      end
+    end
+    root "users#new"
+    resources :sessions, only: [:new, :create, :destroy]
+    resources :users
+    resources :favorites, only: [:create, :destroy, :index]
+    mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
