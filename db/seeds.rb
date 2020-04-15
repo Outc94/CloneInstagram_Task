0df3792c64a330d49ba37df8ec22e37ed3e17593
Mouhamed Diop <outc94@hotmail.fr>
2020-04-16 00:18:42 +0900

    50 seed data for User

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+50.times do |n|
+  full_name = "full_name"
+  username = "username"
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(full_name: full_name,
+               username: username,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               )
+end
