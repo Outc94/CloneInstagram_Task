a20c8ec01c93358da08ef4c7b2a6913ba4f10be5
Mouhamed Diop <outc94@hotmail.fr>
2020-04-16 14:08:10 +0900

    Sending mail to the person who posted

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(post)
     @post = post
-    mail to: "outc94@mail.com", subject: "confirmation e-mail of inquiry"
+    mail to: @post.user.email, subject: "confirmation e-mail of inquiry"
   end
 end
